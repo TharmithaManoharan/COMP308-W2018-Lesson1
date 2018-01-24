c6a24d49973a76acff9b045d0718d37a16aa8d85
TharmithaManoharan <tmanoha4@my.centennialcollege.ca>
2018-01-23 19:53:58 -0500

    added express

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require('http');
+/*const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -11,4 +11,10 @@ const server = http.createServer((req, res) => {
 //listener and inline handler
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-});
+});*/
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
